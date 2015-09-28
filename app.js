eb6a52e983eff15f195e50f409d261af9cf391be
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-28 18:56:31 +0100

    Installed some packages missed and create my first user

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ var debug = require('debug');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var expressLayouts = require('express-ejs-layouts');
 var app = express();
 var router = express.Router();
 
 mongoose.connect('mongodb://localhost/ecommerce_app');
 
 
-var Animal = require('./models/animals')
+var User = require('./models/user')
+
+
+
 
 app.engine('ejs', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 // body parser config
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
@@ -29,6 +34,18 @@ app.use(methodOverride(function (req, res) {
   }
 }));
 
+
+
+var user1 = new User({
+  name: 'John Doe',
+  gender: 'Male',
+  dob: '01/01/2000'
+})
+
+user1.save(function(err, newUser) {
+  if (err) console.log(err);
+  console.log('User1 Saved!');
+})
 
 
 
